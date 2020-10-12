c0ba369e5783013ea0a0523967a4f8777dd35811
sebastientromp <sebastien.tromp@gmail.com>
2020-10-12 22:05:07 +0200

    extract turn computation to lib

--- src/service/game-duration-extractor.ts
+++ src/service/game-duration-extractor.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-use-before-define */
-import { extractTotalDuration, Replay } from '@firestone-hs/hs-replay-xml-parser/dist/public-api';
-import { GameTag } from '@firestone-hs/reference-data';
+import { extractTotalDuration, extractTotalTurns, Replay } from '@firestone-hs/hs-replay-xml-parser/dist/public-api';
 import { ReviewMessage } from '../review-message';
 import { Stat } from '../stat';
 
@@ -10,7 +9,7 @@ export const gameDurationExtractor = async (
 	replayString: string,
 ): Promise<readonly Stat[]> => {
 	const totalDuration = extractTotalDuration(replay);
-	const numberOfTurns = extractNumberOfTurns(replay);
+	const numberOfTurns = extractTotalTurns(replay);
 
 	return [
 		{
@@ -22,11 +21,4 @@ export const gameDurationExtractor = async (
 			statValue: '' + numberOfTurns,
 		} as Stat,
 	];
-};
-
-const extractNumberOfTurns = (replay: Replay): number => {
-	const allTurnChanges = replay.replay.findall(`.//TagChange[@tag='${GameTag.TURN}']`);
-	const lastTurn = allTurnChanges.length > 0 ? allTurnChanges[allTurnChanges.length - 1] : null;
-	const totalTurns = lastTurn ? parseInt(lastTurn.get('value')) : 0;
-	return Math.ceil(totalTurns / 2);
 };
